64be8bad881a4c08c3b447da35a4afbd8e03a79a
Vinh God <vinh572000@gmail.com>
2022-06-05 02:42:40 +0700

    complete product statistic

--- worker/productCompletedChangeStreams.js
+++ worker/productCompletedChangeStreams.js
@@ -10,16 +10,27 @@ const run = async () => {
             const { operationType, fullDocument, updateDescription } = event
 
             const { _id: productId, price } = fullDocument
+            console.log('ok', operationType);
+
             if (
-                ['insert', 'update'].includes(operationType) ||
+                ['insert'].includes(operationType)
+            ) {
+                await ProductStatistics.create({
+                    product: productId,
+                    price: price,
+                    capturedTime: moment(),
+                })
+            } else if (
+                ['update'].includes(operationType) &&
                 updateDescription?.updatedFields?.price
-            )
+            ) {
                 await ProductStatistics.findOneAndUpdate(
+                    //trong cung 1 ngay gia nho hon thi k lmj gia lon hon thi ms cap nhat
                     {
                         product: productId,
-                        price: { $lt: price },
+                        // price: { $lt: price },//tìm tất cả sp có giá nhỏ hơn sp truyền zô
                         capturedTime: {
-                            $gte: moment().startOf('day'),
+                            $gte: moment().startOf('day'),//ngày lớn hơn or= ngày hiện tại
                         },
                     },
                     {
@@ -28,40 +39,9 @@ const run = async () => {
                             capturedTime: moment(),
                         },
                     },
-                    { upsert: operationType === 'insert' }
+                    { upsert: operationType === 'update' }
                 )
-
-            // if (
-            //     ['insert'].includes(operationType)
-            // ) {
-            //     await ProductStatistics.create({
-            //         product: productId,
-            //         price: price,
-            //         capturedTime: moment(),
-            //     })
-            // }
-
-            // if (
-            //     ['update'].includes(operationType) &&
-            //     updateDescription?.updatedFields?.price
-            // ) {
-            //     await ProductStatistics.findOneAndUpdate(
-            //         {
-            //             product: productId,
-            //             price: { $lt: price },//nhỏ hơn
-            //             capturedTime: {
-            //                 $gte: moment().startOf('day'),//lớn hơn hoặc bằng
-            //             },
-            //         },
-            //         {
-            //             $set: {
-            //                 price,
-            //                 capturedTime: moment(),
-            //             },
-            //         },
-            //         { upsert: operationType === 'update' }
-            //     )
-            // }
+            }
 
         } catch (error) {
             console.log(error)
